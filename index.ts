95ede32fb7e6cbaee438b612429bc947438a7479
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-03-08 22:10:45 +0700

    8.2. Lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("OK");
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import * as database from './config/database';
+import Task from './models/task.model';
+
+dotenv.config();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+database.connect();
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false
+  });
+
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
