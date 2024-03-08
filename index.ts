0a87efa1c9504d45c2b756370ddbef52d4b6a04f
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-03-08 22:35:02 +0700

    9.1. Tạo routes

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import * as database from './config/database';
-import Task from './models/task.model';
+import mainV1Routes from './api/v1/routes/index.route';
 
 dotenv.config();
 
@@ -10,23 +10,8 @@ const port: number | string = process.env.PORT || 3000;
 
 database.connect();
 
-app.get("/tasks", async (req: Request, res: Response) => {
-  const tasks = await Task.find({
-    deleted: false
-  });
-
-  res.json(tasks);
-});
-
-app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
-  const id: string = req.params.id;
-  const task = await Task.findOne({
-    _id: id,
-    deleted: false
-  });
-
-  res.json(task);
-});
+// Route v1
+mainV1Routes(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
