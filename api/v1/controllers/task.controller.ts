b118bcdfaef9cb9321cd5f07f44e192f77391356
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-03-09 21:39:44 +0700

    12. Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,19 +3,30 @@ import { Request, Response } from 'express';
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+  // find
   interface Find{
     deleted: boolean,
     status?: string
   }
-  
+
   const find: Find = {
     deleted: false
   };
   if(req.query.status) {
     find.status = req.query.status.toString();
   }
+  // end find
 
-  const tasks = await Task.find(find);
+  // sort
+  const sort = {};
+  if(req.query.sortKey && req.query.sortValue){
+    const sortKey = req.query.sortKey.toString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  // console.log(sort)
+  // end sort 
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 }
