1b1fef4b0fc5dcb815d9b2586ffa73b9fd41107a
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-03-13 23:44:46 +0700

    7. Xóa công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -164,3 +164,24 @@ export const edit = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+
+    await Task.updateOne({ _id: id }, {
+      deleted: true,
+      deletedAt: new Date()
+    });
+
+    res.json({
+      code: 200,
+      message: "Successfully!"
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Falied!"
+    });
+  }
+};
