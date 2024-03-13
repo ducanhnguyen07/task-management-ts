c7f829029ffe64c75f5b5ae72d75aafc49e9df32
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-03-13 23:30:13 +0700

    5. Thêm mới công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -128,3 +128,21 @@ export const changeMulti = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const create = async (req: Request, res: Response) => {
+  try {
+    const task = new Task(req.body);
+    const data = await task.save();
+
+    res.json({
+      code: 200,
+      message: "Successfully!",
+      data: data
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Failed!"
+    });
+  }
+};
