9207e841482e8e7b39a95e7be94784cd4f72be59
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-03-14 22:36:32 +0700

    14. Chuyển các route thành private

--- api/v1/controllers/user.controller.ts
+++ api/v1/controllers/user.controller.ts
@@ -74,16 +74,9 @@ export const login = async (req: Request, res: Response) => {
 
 // [POST] /api/v1/user/detail
 export const detail = async (req: Request, res: Response) => {
-  const id: string = req.params.id;
-
-  const user = await User.findOne({
-    _id: id,
-    deleted: false
-  }).select("-password -token");
-
   res.json({
     code: 200,
     message: "Successfully!",
-    info: user
+    info: req["infoUser"]
   });
 };
